a763b8548d079bcc870b2b199756f724b3adeb3e
Aldy Akbarrizky <AldyAkbarrizky18@gmail.com>
2022-05-22 17:19:40 +0700

    Change database from local to remote

--- cms/config/database.js
+++ cms/config/database.js
@@ -2,11 +2,11 @@ module.exports = ({ env }) => ({
   connection: {
     client: 'mysql',
     connection: {
-      host: env('DATABASE_HOST', '127.0.0.1'),
+      host: env('DATABASE_HOST', 'db4free.net'),
       port: env.int('DATABASE_PORT', 3306),
-      database: env('DATABASE_NAME', 'web-jtk'),
-      user: env('DATABASE_USERNAME', 'root'),
-      password: env('DATABASE_PASSWORD', ''),
+      database: env('DATABASE_NAME', 'web_jtk'),
+      user: env('DATABASE_USERNAME', 'jtk_admin'),
+      password: env('DATABASE_PASSWORD', 'Bandung1999'),
       ssl: false
     },
   },
