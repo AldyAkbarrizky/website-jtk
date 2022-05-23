f4bfa2926006104de7a8f48059ca310af409c679
Aldy Akbarrizky <AldyAkbarrizky18@gmail.com>
2022-05-23 07:22:45 +0700

    Update config for heroku

--- cms/config/database.js
+++ cms/config/database.js
@@ -2,11 +2,11 @@ module.exports = ({ env }) => ({
   connection: {
     client: 'mysql',
     connection: {
-      host: env('DATABASE_HOST', 'db4free.net'),
-      port: env.int('DATABASE_PORT', 3306),
-      database: env('DATABASE_NAME', 'web_jtk'),
-      user: env('DATABASE_USERNAME', 'jtk_admin'),
-      password: env('DATABASE_PASSWORD', 'Bandung1999'),
+      host: env('DATABASE_HOST'),
+      port: env.int('DATABASE_PORT'),
+      database: env('DATABASE_NAME'),
+      user: env('DATABASE_USERNAME'),
+      password: env('DATABASE_PASSWORD'),
       ssl: false
     },
   },
